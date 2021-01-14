a21336df7bcbc78e9b107971255ff8e1b07f8caa
nmnithinkrishna <nmnithinkrishna@gmail.com>
2021-01-14 23:21:19 +0530

    Add Expensify Status

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import numeral from 'numeral';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     const expenseWord = expensesCount === 1 ? 'expense' : 'expenses' ;
     return (
-    <div>
-        <h1>Viewing {expensesCount} {expenseWord} totalling {numeral(expensesTotal / 100).format('$0,0.00')}</h1>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span></h1>
+            <div className="page-header__actions">
+                <Link className="button" to="/create">Add Expense</Link>
+            </div>
+        </div>
     </div>
 )};
 
